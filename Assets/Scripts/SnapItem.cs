28ed914990f65596d8e6f93e6a519414acdaf678
LithosphereRocketry <greatmaharg@gmail.com>
2020-11-19 13:34:34 -0500

    Added snap system

---
+++ Assets/Scripts/SnapItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void ShortClicked(RaycastHit target) {
+		GameObject node = target.collider.gameObject;
+		if(node.GetComponent("NodeSnap")) {
+			gameObject.transform.SetParent(node.transform, false);
+		}
+	}
+}
