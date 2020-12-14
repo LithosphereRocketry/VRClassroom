7764d5a53c3fc0a53ae9a4110da91a039265735d
LithosphereRocketry <greatmaharg@gmail.com>
2020-12-14 13:18:15 -0500

    table snapping

---
+++ Assets/Scripts/FloorItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorItem : PickupItem {
+    void ShortClicked(RaycastHit target) {
+		GameObject node = target.collider.gameObject;
+		if(node.GetComponent("FloorSnap")) {
+			rb.isKinematic = true;
+			Vector3 lookDir = gameObject.transform.parent.parent.position;
+			gameObject.transform.SetParent(null);
+			gameObject.transform.position = target.point;
+			lookDir.y = gameObject.transform.position.y;
+			gameObject.transform.LookAt(lookDir, Vector3.up);
+			
+		}
+	}
+}
