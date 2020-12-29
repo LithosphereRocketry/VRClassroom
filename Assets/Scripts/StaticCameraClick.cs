a6aea5295fedcac938b10293cd97d8966de12817
Ben Graham <greatmaharg@gmail.com>
2020-12-29 11:43:37 -0500

    squash

---
+++ Assets/Scripts/StaticCameraClick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class StaticCameraClick : MonoBehaviour
+{
+    public GameObject hand;
+	public GameObject crosshair;
+    private Camera cam;
+	private bool enabled;
+	
+	void Start() {
+		enabled = !XRSettings.enabled;
+        cam = Camera.main;
+		if(!hand) { hand = gameObject; }
+		if(enabled) {
+			Destroy(crosshair);
+			Cursor.lockState = CursorLockMode.None;
+		}
+    }
+
+	
+	void Update() {
+		if(enabled && Input.GetMouseButtonDown(0)) {
+			Ray look = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit lookPt;
+			if(Physics.Raycast(look, out lookPt)) {
+				lookPt.collider.gameObject.SendMessage("WorldClicked", hand, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
