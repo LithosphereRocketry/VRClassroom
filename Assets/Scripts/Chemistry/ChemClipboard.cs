898b3ecf80e851a489f64d821f3c4bd38a10048f
LithosphereRocketry <greatmaharg@gmail.com>
2021-02-01 13:42:11 -0500

    tiles

---
+++ Assets/Scripts/Chemistry/ChemClipboard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChemClipboard : MonoBehaviour
+{
+    public GameObject textCanvas;
+	Text text;
+	string molecule = "";
+	
+	void Start() {
+		text = textCanvas.GetComponent<Text>();
+	}
+    
+	void ShortClicked(RaycastHit point) {
+		ChemSource src = (ChemSource) point.collider.gameObject.GetComponent("ChemSource");
+        if(src) {
+			molecule += src.element;
+			text.text = molecule;
+			Debug.Log(subscript(1234));
+		}
+    }
+	
+	string subscript(int n) {
+		char d = (char) (0x2080 + n%10);
+		if(n >= 10) {
+			return subscript(n/10)+d.ToString();
+		} else {
+			return d.ToString();
+		}
+	}
+}
--- Assets/Scripts/Chemistry/ChemSource.cs
+++ Assets/Scripts/Chemistry/ChemSource.cs
@@ -2,18 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChemSource : MonoBehaviour
-{
+public class ChemSource : MonoBehaviour {
 	public string element;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public GameObject tile;
+	void Start() {
+		tile.GetComponent<Renderer>().material = Resources.Load("ElementTiles/Materials/"+element, typeof(Material)) as Material;
+	}
 }
