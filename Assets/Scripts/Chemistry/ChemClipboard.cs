4c3261292c4fe51176620ba208d726828f404725
LithosphereRocketry <greatmaharg@gmail.com>
2021-02-04 13:41:35 -0500

    clipboard next step

--- Assets/Scripts/Chemistry/ChemClipboard.cs
+++ Assets/Scripts/Chemistry/ChemClipboard.cs
@@ -16,11 +16,44 @@ public class ChemClipboard : MonoBehaviour
 	void ShortClicked(RaycastHit point) {
 		ChemSource src = (ChemSource) point.collider.gameObject.GetComponent("ChemSource");
         if(src) {
-			molecule += src.element;
-			text.text = molecule;
-			Debug.Log(subscript(1234));
+			molecule = addAtom(src.element, molecule);
+			text.text = makeReadable(molecule);
 		}
     }
+	
+	string addAtom(string atom, string mol) {
+		string[] molbreak = molecule.Split(' ');
+		string output = "";
+		bool placed = false;
+		for(int i = 0; i < molbreak.Length; i++) {
+			string[] atombreak = molbreak[i].Split('_');
+			if(atombreak.Length > 1) {
+				if(atombreak[0] == atom) {
+					atombreak[1] = (int.Parse(atombreak[1]) + 1).ToString();
+					placed = true;
+				}
+				output += (atombreak[0]+"_"+atombreak[1]+" ");
+			}
+		}
+		if(!placed) {
+			output += atom+"_1 ";
+		}
+		return output;
+	}
+	
+	string makeReadable(string molecule) {
+		string[] molbreak = molecule.Split(' ');
+		string output = "";
+		for(int i = 0; i < molbreak.Length; i++) {
+			string[] atombreak = molbreak[i].Split('_');
+			if(atombreak.Length > 1) {
+				output += char.ToUpper(atombreak[0][0]) + atombreak[0].Substring(1);
+				int num = int.Parse(atombreak[1]);
+				output += num>1 ? subscript(num) : "";
+			}
+		}
+		return output;
+	}
 	
 	string subscript(int n) {
 		char d = (char) (0x2080 + n%10);
