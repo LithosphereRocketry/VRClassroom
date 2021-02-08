60c6a8f69086439de2ef927fcef0b0f9b00f3b46
LithosphereRocketry <greatmaharg@gmail.com>
2021-02-08 13:38:48 -0500

    better clipboard handling

--- Assets/Scripts/Chemistry/ChemClipboard.cs
+++ Assets/Scripts/Chemistry/ChemClipboard.cs
@@ -3,58 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChemClipboard : MonoBehaviour
+public class ChemClipboard : ChemInfo
 {
     public GameObject textCanvas;
 	Text text;
-	string molecule = "";
+	
+	List<Element> elements = new List<Element>();
+	List<int> counts = new List<int>();
 	
 	void Start() {
+		base.Start();
 		text = textCanvas.GetComponent<Text>();
 	}
     
 	void ShortClicked(RaycastHit point) {
-		ChemSource src = (ChemSource) point.collider.gameObject.GetComponent("ChemSource");
+		GameObject node = point.collider.gameObject;
+		
+		ChemSource src = (ChemSource) node.GetComponent("ChemSource");
         if(src) {
-			molecule = addAtom(src.element, molecule);
-			text.text = makeReadable(molecule);
+			//molecule = addAtom(src.element.abbr, molecule);
+			addAtomDynamic(src.element);
+			text.text = makeReadableDynamic();
+		}
+		
+		if(node.GetComponent("TrashCan")) {
+			elements = new List<Element>();
+			counts = new List<int>();
+			text.text = "";
 		}
     }
 	
-	string addAtom(string atom, string mol) {
-		string[] molbreak = molecule.Split(' ');
-		string output = "";
-		bool placed = false;
-		for(int i = 0; i < molbreak.Length; i++) {
-			string[] atombreak = molbreak[i].Split('_');
-			if(atombreak.Length > 1) {
-				if(atombreak[0] == atom) {
-					atombreak[1] = (int.Parse(atombreak[1]) + 1).ToString();
-					placed = true;
-				}
-				output += (atombreak[0]+"_"+atombreak[1]+" ");
+	void addAtomDynamic(Element atom) {
+		for(int i = 0; i < elements.Count; i++) {
+			if(elements[i] == atom) {
+				counts[i] ++;
+				return;
 			}
 		}
-		if(!placed) {
-			output += atom+"_1 ";
+		elements.Add(atom);
+		counts.Add(1);
+	}
+	string makeReadableDynamic() {
+		string output = "";
+		for(int i = 0; i < elements.Count; i++) {
+			output += char.ToUpper(elements[i].abbr[0]) + elements[i].abbr.Substring(1) + (counts[i] > 1 ? subscript(counts[i]) : "");
 		}
 		return output;
 	}
-	
-	string makeReadable(string molecule) {
-		string[] molbreak = molecule.Split(' ');
-		string output = "";
-		for(int i = 0; i < molbreak.Length; i++) {
-			string[] atombreak = molbreak[i].Split('_');
-			if(atombreak.Length > 1) {
-				output += char.ToUpper(atombreak[0][0]) + atombreak[0].Substring(1);
-				int num = int.Parse(atombreak[1]);
-				output += num>1 ? subscript(num) : "";
-			}
-		}
-		return output;
-	}
-	
 	string subscript(int n) {
 		char d = (char) (0x2080 + n%10);
 		if(n >= 10) {
--- Assets/Scripts/Chemistry/ChemSource.cs
+++ Assets/Scripts/Chemistry/ChemSource.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChemSource : MonoBehaviour {
-	public string element;
+public class ChemSource : ChemInfo {
+	public int elementNum;
+	public Element element;
 	public GameObject tile;
 	void Start() {
-		tile.GetComponent<Renderer>().material = Resources.Load("ElementTiles/Materials/"+element, typeof(Material)) as Material;
+		base.Start();
+		element = table[elementNum-1];
+		Debug.Log(element);
+		tile.GetComponent<Renderer>().material = Resources.Load("ElementTiles/Materials/"+element.abbr, typeof(Material)) as Material;
 	}
 }
