4ef86a4d3a847d8586f7ec981eace038496893be
LithosphereRocketry <greatmaharg@gmail.com>
2021-03-11 13:34:19 -0500

    ramp and cylinder

---
+++ Assets/Scripts/ObjectUtils/CustomMoment.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomMoment : MonoBehaviour
+{
+    public Vector3 moment;
+	
+	void Start() {
+        GetComponent<Rigidbody>().inertiaTensor = moment;
+    }
+}
