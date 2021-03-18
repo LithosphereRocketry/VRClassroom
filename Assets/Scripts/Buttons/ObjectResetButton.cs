d7d425a13d48d7b35c49b605f009c12106eacd5a
LithosphereRocketry <greatmaharg@gmail.com>
2021-03-18 13:34:55 -0400

    reset

---
+++ Assets/Scripts/Buttons/ObjectResetButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectResetButton : MonoBehaviour
+{
+    public GameObject target;
+	public Vector3 resetPosition;
+	public GameObject timer;
+	private Rigidbody body;
+	
+	void Start() {
+		body = target.GetComponent<Rigidbody>();
+	}
+	
+	void WorldClicked() {
+        body.position = resetPosition;
+		body.velocity = Vector3.zero;
+		body.angularVelocity = Vector3.zero;
+		if(timer) {
+			timer.SendMessage("StartTimer", SendMessageOptions.DontRequireReceiver);
+		}
+    }
+}
