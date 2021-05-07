325fe48b1a7e746c3db1d0f0f78e1e41c31722ee
Ben Graham <greatmaharg@gmail.com>
2021-05-07 15:28:42 -0400

    trash model

--- Assets/Scripts/ItemManipulation/TrashCan.cs
+++ Assets/Scripts/ItemManipulation/TrashCan.cs
@@ -2,4 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrashCan : MonoBehaviour {}
+public class TrashCan : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		GameObject item = other.gameObject;
+		if(item.GetComponent("TrashableItem") && (item.GetComponent<Rigidbody>() && !item.GetComponent<Rigidbody>().isKinematic)) {
+			Destroy(other.gameObject);
+		}
+	}
+}
