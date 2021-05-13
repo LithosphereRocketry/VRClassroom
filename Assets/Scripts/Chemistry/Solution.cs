fcfd6ff021dc2d6b892787cbfbceccdaa6c8ee15
unknown <greatmaharg@gmail.com>
2021-05-13 13:35:02 -0400

    beaker model

---
+++ Assets/Scripts/Chemistry/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solution : IonInfo
+{
+    float emptyMass;
+	public float maxVolume;
+	float waterVolume;
+	public GameObject water;
+	public GameObject solids;
+	
+	List<int> cations = new List<int>();
+	List<int> anions = new List<int>();
+	
+	class Compound {
+		float solubility;
+		float mass;
+		Compound(int cat, int an) {
+			
+		}
+	}
+	
+	void Start() {
+        waterVolume = 0;
+    }
+
+    void Update() {
+        
+    }
+}
